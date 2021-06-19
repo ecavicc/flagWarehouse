cbf8d1b1f33233d627dd953da423902329029633
ecavicc <33295173+ecavicc@users.noreply.github.com>
2021-06-19 12:58:12 +0200

    Edited config with correct parameters

--- server/config.py
+++ server/config.py
@@ -2,18 +2,18 @@ class Config(object):
     WEB_PASSWORD = 'password'
     API_TOKEN = 'token'
 
-    YOUR_TEAM = '10.0.2.1'
-    TEAM_TOKEN = 'ThIsIsAtEaMtOkEn'
-    TEAMS = ['10.0.{}.1'.format(i) for i in range(1, 30)]
+    YOUR_TEAM = '10.60.27.1'
+    TEAM_TOKEN = '8a8d25f465bec01d'
+    TEAMS = ['10.60.{}.1'.format(i) for i in range(1, 35)]
     TEAMS.remove(YOUR_TEAM)
 
-    ROUND_DURATION = 60
+    ROUND_DURATION = 120
     FLAG_ALIVE = 5 * ROUND_DURATION
-    FLAG_FORMAT = r'FLAG\{[A-Za-z0-9]{25}\}'
+    FLAG_FORMAT = r'/^[A-Z0-9]{31}=$/'
 
-    SUB_LIMIT = 50
+    SUB_LIMIT = 10
     SUB_INTERVAL = 1
-    SUB_URL = 'http://localhost:8000'
+    SUB_URL = 'http://10.1.0.2/flags'
     SUB_ACCEPTED = 'accepted'
     SUB_ERROR = 'error'
 
